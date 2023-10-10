cfb29ddff924a53315aad2517d49afff5a08e9c2
davem <davideallega@gmail.com>
2023-10-11 00:12:53 +0200

    example of helper

--- public/index.php
+++ public/index.php
@@ -2,10 +2,13 @@
 
 require "../bootstrap.php";
 
+use Slim\Http\Request;
+use Slim\Http\Response;
+
 $app = require "../bootstrap.php"; // Carica l'applicazione Slim creata in bootstrap.php
 
-$app->get('/', function($request, $response) {
-    $response->getBody()->write('home');
+$app->get('/', function(Request $request, Response $response, array $args) {
+    $response->getBody()->write(dd($request));
     return $response;
 });
 
